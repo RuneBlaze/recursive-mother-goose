144e0a40d432f99e5cd43f23cc8490c6bd9cce8b
Runeblaze <facss_carpool_service@unc.edu>
2019-03-24 16:30:20 -0400

    updated tester

--- tester.sh
+++ tester.sh
@@ -1,23 +1,32 @@
+declare -i total=0
+declare -i incorrect=0
+
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
 for i in id_cases/*_ac.java; do
     [ -f "$i" ] || break
+    let "total=total+1"
     echo "========$i========"
     java miniJava.Compiler $i
     if [ "$?" -ne 0 ]; then
         echo "${red} $i not passed: $? ${reset}"
+        let "incorrect=incorrect+1"
     fi
     # echo "================="
 done
 
 for i in id_cases/*_wa.java; do
     [ -f "$i" ] || break
+    let "total=total+1"
     echo "=======$i========"
     java miniJava.Compiler $i
     if [ "$?" = 0 ]; then
         echo "${red} $i not passed: $? ${reset}"
+        let "incorrect=incorrect+1"
     fi
     # echo "================="
 done
+
+echo "Failed Tests: $incorrect, Total $total"
